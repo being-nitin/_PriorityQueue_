6e39df8e407b165ebfe971646a904f7528ef65dd
Nitin <nsinghboss02@gmail.com>
2022-03-01 21:55:24 +0530

    "offer method explained"

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	With this we can set a priority for a particular element.
 	 */
     PriorityQueue <Integer> pq = new PriorityQueue<>();
+    // offer method will add an element in a priority queue.
         pq.offer(90);
         pq.offer(78);
         pq.offer(87);
