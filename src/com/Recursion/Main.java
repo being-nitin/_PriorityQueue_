8e18859699730f532e8a008205569324b09259a2
Nitin <nsinghboss02@gmail.com>
2022-03-01 21:54:26 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,19 @@
+package com.Recursion;
+import java.util.PriorityQueue;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Priority queue with its methods.
+	With this we can set a priority for a particular element.
+	 */
+    PriorityQueue <Integer> pq = new PriorityQueue<>();
+        pq.offer(90);
+        pq.offer(78);
+        pq.offer(87);
+        pq.offer(94);
+        pq.offer(98);
+        pq.poll();
+        System.out.println(pq);
+    }
+}
